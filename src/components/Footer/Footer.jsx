4a24128f9383eb8fda12f51986a2376d1109eed0
Jeremy Rice <jdavidrice@gmail.com>
2021-03-30 20:41:44 -0400

    Add social links and edit footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -38,7 +38,7 @@ const Footer = () => {
         </div>
         <hr />
         <p className="footer__text">
-          © {new Date().getFullYear()} - Template developed by{' '}
+          © {new Date().getFullYear()} - Jeremy Rice <br /> Adapted from a design by{' '}
           <a href="https://github.com/cobidev" target="_blank" rel="noopener noreferrer">
             Jacobo Martínez
           </a>
--- src/mock/data.js
+++ src/mock/data.js
@@ -29,11 +29,38 @@ export const aboutData = {
   img: 'profile.png',
   paragraphOne:
     "After being a teacher for fifteen years, I've decided to leave the world of public education and to pursue a career in web development. I have built this website to collect and display some examples of my work. I am currently attending the UCONN Coding Boot Camp: Full Stack Flex Program, and will complete the course in April 2021. In my spare time, I enjoy playing the guitar, drums, mandolin, and saxophone, and learning about science, history, technology, philosophy, and meditation.",
-  resume: '../documents/Jeremy-Rice.pdf', // if no resume, the button will not show up
+  resume: 'https://drive.google.com/file/d/1hovx5wP5SHD8VQ493U6MhhVdN3wCbw64/view?usp=sharing',
 };
 
 // PROJECTS DATA
 export const projectsData = [
+  {
+    id: nanoid(),
+    img: 'project.jpg',
+    title: '',
+    info: '',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'project.jpg',
+    title: '',
+    info: '',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'project.jpg',
+    title: '',
+    info: '',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
   {
     id: nanoid(),
     img: 'project.jpg',
@@ -75,23 +102,18 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
-    {
-      id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/jeremy-rice-b67a531b4/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/jdavidrice',
+    },
+    {
+      id: nanoid(),
+      name: 'coffee',
+      url: 'https://www.buymeacoffee.com/jdavidrice',
     },
   ],
 };
